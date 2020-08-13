5bc8afd7f1caad83e148a041dc1a22a9ff178661
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-13 03:33:44 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,15 @@
 import React, { Component } from 'react'
+
+export default class SimpleComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      this.mood = 'happy';
+    };
+  }
+
+  render() {
+    <div this.mood></div>
+  }
+  
+}
